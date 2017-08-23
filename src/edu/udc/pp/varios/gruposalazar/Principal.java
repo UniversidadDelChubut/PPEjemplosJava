d62a79f155dfad90b718c187ea44eabf2e073280
Santiago Della Torre <sadellatorre@issys.gov.ar>
2017-08-23 11:07:32 -0300

    Se unifican ejemplos

--- src/edu/udc/pp/varios/gruposalazar/Principal.java
+++ src/edu/udc/pp/varios/gruposalazar/Principal.java
@@ -1,11 +1,12 @@
 package edu.udc.pp.varios.gruposalazar;
 
 public class Principal {
-
+	
 	public static void main(String[] args) {
 		
 		AgrupadorAlAzar agrupador = new AgrupadorAlAzar("estudiantes.txt");
 		agrupador.agruparEImprimir(2);
 		
 	}
+	
 }
