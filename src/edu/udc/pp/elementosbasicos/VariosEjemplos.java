3b0cefc3cb9c30aff324c07d548ad99c1a81ee4c
universidad <sdellatorre@github.com>
2017-09-07 20:16:13 -0300

    Se agrega ejemplo de generacion de clave aleatoria

--- src/edu/udc/pp/elementosbasicos/VariosEjemplos.java
+++ src/edu/udc/pp/elementosbasicos/VariosEjemplos.java
@@ -1,10 +1,8 @@
 package edu.udc.pp.elementosbasicos;
 
+import java.util.Random;
+
 public class VariosEjemplos {
-
-	
-
-	   
 	   
     public static int numeroAstral (String nombre) {
        
@@ -28,12 +26,52 @@ public class VariosEjemplos {
     }
     
     public static String claveAlAzar(int longitud, boolean conMayusculas, boolean conNumeros) {
-    	return "bhsghga";
+    	
+    	String clave ="";
+    	Random random = new Random();
+    	
+    	for (int i = 0; i < longitud ; i++) {
+    		
+			char c = '\0';    		
+    		
+			//Tipo
+			int opcion = random.nextInt(3);
+			
+			if (!conNumeros && opcion == 2){
+				opcion = random.nextInt(2);
+			} 
+			
+			if (!conMayusculas && opcion == 1){
+				opcion = 0;
+			} 
+			
+			
+			
+			
+    		switch (opcion) {
+			case 0:
+				c = (char)('a' + random.nextInt(26));
+				break;
+			case 1:
+				c = (char)('A' + random.nextInt(26));
+				break;
+			case 2:
+				c = (char)('0' + random.nextInt(10));
+				break;
+
+			}
+ 
+			clave = clave + c;
+		}
+    	
+    	
+    	return clave;
     }
    
    
     public static void main(String[] args) {
         System.out.println( numeroAstral("silvia"));
+        System.out.println( claveAlAzar(8, true, false));
     }
 	
 }
